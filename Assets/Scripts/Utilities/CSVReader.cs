58b3a01014da0e7d76e028a27268b2d6d5211c23
deliafH <huytq.workplace@gmail.com>
2024-07-09 21:43:28 +0700

    ~path, read File CSV

--- Assets/Scripts/Utilities/CSVReader.cs
+++ Assets/Scripts/Utilities/CSVReader.cs
@@ -4,33 +4,61 @@ using System.IO;
 
 public class CSVReader : MonoBehaviour
 {
-    public string filePath = "Data/Map/Map_1.csv"; // Path to the CSV file
-/*
+    public string filePath = "CSV_Map/Map_1.csv"; // Relative path to the CSV file within StreamingAssets
+
     void Start()
     {
         List<List<int>> matrix = LoadCSV(filePath);
+
         // Output to debug the matrix content
         foreach (List<int> row in matrix)
         {
             Debug.Log(string.Join(", ", row));
         }
-    }*/
+    }
 
     public static List<List<int>> LoadCSV(string relativePath)
     {
         // Combine the relative path with the data path
-        string fullPath = Path.Combine(Application.dataPath, relativePath);
-        if (!File.Exists(fullPath))
+        string fullPath = Path.Combine(Application.streamingAssetsPath, relativePath);
+        string csvData = null;
+
+        if (Application.platform == RuntimePlatform.Android)
         {
-            Debug.LogError("File not found: " + fullPath);
-            return new List<List<int>>(); // Return an empty list to avoid further errors
+            // Android requires using UnityWebRequest to read from StreamingAssets synchronously
+            using (UnityEngine.Networking.UnityWebRequest reader = UnityEngine.Networking.UnityWebRequest.Get(fullPath))
+            {
+                reader.SendWebRequest();
+                while (!reader.isDone) { }
+                if (reader.result == UnityEngine.Networking.UnityWebRequest.Result.ConnectionError || reader.result == UnityEngine.Networking.UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.LogError("Failed to read file: " + reader.error);
+                    return new List<List<int>>(); // Return an empty list to avoid further errors
+                }
+                csvData = reader.downloadHandler.text;
+            }
+        }
+        else
+        {
+            if (!File.Exists(fullPath))
+            {
+                //Debug.LogError("File not found: " + fullPath);
+                return new List<List<int>>(); // Return an empty list to avoid further errors
+            }
+            csvData = File.ReadAllText(fullPath);
         }
 
+        return ParseCSV(csvData);
+    }
+
+    private static List<List<int>> ParseCSV(string csvData)
+    {
         List<List<int>> matrix = new List<List<int>>();
-        string[] lines = File.ReadAllLines(fullPath);
+        string[] lines = csvData.Split('\n');
 
         foreach (string line in lines)
         {
+            if (string.IsNullOrWhiteSpace(line)) continue; // Skip empty lines
             List<int> row = new List<int>();
             string[] entries = line.Split(',');
             foreach (string entry in entries)
